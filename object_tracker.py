7de528c9ad67638bb290b764ff8a2063836e1c59
tdh <tdhster@gmail.com>
2024-05-07 20:54:27 +0300

    add tracking mosse (not work) add centroidtracking.py from pypi

---
+++ object_tracker.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+
+class MOSSETracker:
+    def __init__(self, adaptive=False):
+        self.adaptive = adaptive
+        self.mosse = cv2.legacy.TrackerMOSSE_create()
+        self.next_object_id = 0
+        self.objects = {}
+
+    def init(self, frame, bbox):
+        """Initializes the tracker with the first frame and bounding box."""
+        object_id = self.next_object_id
+        ok = self.mosse.init(frame, bbox)
+        self.objects[object_id] = bbox
+        self.next_object_id += 1
+        return ok, object_id
+
+    def update(self, bboxes):
+        """Updates the tracker's state with the current frame and array of bounding boxes."""
+        object_ids = list(self.objects.keys())
+        for object_id in object_ids:
+            bbox = self.objects[object_id]
+            ok, new_bbox = self.mosse.update(bboxes)
+            if ok:
+                self.objects[object_id] = new_bbox
+            else:
+                del self.objects[object_id]
+
+    def get_object_id(self, bbox):
+        """Returns the object ID associated with the given bounding box."""
+        for object_id, bbox_ref in self.objects.items():
+            if np.array_equal(bbox, bbox_ref):
+                return object_id
+        return -1
+
+
+
+
+
+
+
+class CentroidTracker:
+    def __init__(self, max_disappeared_frames=50, distance_threshold=50):
+        self.next_object_id = 0
+        self.objects = {}
+        self.disappeared = {}
+        self.disappeared_frames = {}
+        self.max_disappeared_frames = max_disappeared_frames
+        self.distance_threshold = distance_threshold
+
+    def register(self, centroid):
+        """Registers a new object with its centroid."""
+        self.objects[self.next_object_id] = centroid
+        self.disappeared_frames[self.next_object_id] = 0
+        self.next_object_id += 1
+
+    def deregister(self, object_id):
+        """Deregisters an object from tracking."""
+        del self.objects[object_id]
+        del self.disappeared_frames[object_id]
+
+    def update(self, rects):
+        """Updates the tracks of detected objects."""
+        new_objects = {}
+        for i, (x, y, w, h) in enumerate(rects):
+            centroid = (int((x + x + w) / 2), int((y + y + h) / 2))
+            object_id = -1
+            for id, center in self.objects.items():
+                # Convert tuples to NumPy arrays for element-wise subtraction
+                center_array = np.array(center)
+                centroid_array = np.array(centroid)
+                distance = np.linalg.norm(center_array - centroid_array)
+                if distance < self.distance_threshold:
+                    object_id = id
+                    self.disappeared[id] = 0
+                    break
+            if object_id < 0:
+                self.register(centroid)
+                object_id = self.next_object_id - 1
+
+            new_objects[object_id] = centroid
+
+        self.objects = new_objects
+        print(f'{new_objects=}')
+        return new_objects
--- track_targets.py
+++ track_targets.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from object_tracker import MOSSETracker as ObjectTracker
 
 # Загружаем каскадную модель Хаара
 # https://github.com/opencv/opencv/tree/master/data/haarcascades
@@ -35,50 +36,6 @@ configPath = 'neuronet/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'neuronet/frozen_inference_graph.pb'
 
 
-class CentroidTracker:
-    def __init__(self, max_disappeared_frames=50, distance_threshold=50):
-        self.next_object_id = 0
-        self.objects = {}
-        self.disappeared = {}
-        self.disappeared_frames = {}
-        self.max_disappeared_frames = max_disappeared_frames
-        self.distance_threshold = distance_threshold
-
-    def register(self, centroid):
-        """Registers a new object with its centroid."""
-        self.objects[self.next_object_id] = centroid
-        self.disappeared_frames[self.next_object_id] = 0
-        self.next_object_id += 1
-
-    def deregister(self, object_id):
-        """Deregisters an object from tracking."""
-        del self.objects[object_id]
-        del self.disappeared_frames[object_id]
-
-    def update(self, rects):
-        """Updates the tracks of detected objects."""
-        new_objects = {}
-        for i, (x, y, w, h) in enumerate(rects):
-            centroid = (int((x + x + w) / 2), int((y + y + h) / 2))
-            object_id = -1
-            for id, center in self.objects.items():
-                # Convert tuples to NumPy arrays for element-wise subtraction
-                center_array = np.array(center)
-                centroid_array = np.array(centroid)
-                distance = np.linalg.norm(center_array - centroid_array)
-                if distance < self.distance_threshold:
-                    object_id = id
-                    self.disappeared[id] = 0
-                    break
-            if object_id < 0:
-                self.register(centroid)
-                object_id = self.next_object_id - 1
-
-            new_objects[object_id] = centroid
-
-        self.objects = new_objects
-        print(f'{new_objects=}')
-        return new_objects
 
 
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
@@ -87,7 +44,35 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-object_tracker = CentroidTracker(max_disappeared_frames=10, distance_threshold=20)
+
+class MOSSETracker:
+    def __init__(self, frame, bbox):
+        self.tracker = cv2.legacy.TrackerMOSSE_create()
+        self.tracker.init(frame, bbox)
+        self.object_id = 0
+        self.object_ids = {}
+
+    def track(self, frame):
+        ret, bbox = self.tracker.update(frame)
+        if ret:
+            if self.object_id not in self.object_ids:
+                self.object_ids[self.object_id] = bbox
+                self.object_id += 1
+        return ret, bbox
+# object_tracker = CentroidTracker(max_disappeared_frames=10, distance_threshold=20)
+# object_tracker = ObjectTracker()
+
+
+ret, frame = cap.read()
+if not ret:
+    print("Error: Unable to read the video")
+    exit()
+
+frame = cv2.resize(frame, (320, 200), interpolation=cv2.INTER_AREA)
+bbox = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+# Initialize MOSSE tracker
+mosse_tracker = MOSSETracker(frame, bbox)
+
 
 while True:
     success, frame = cap.read()
@@ -96,20 +81,28 @@ while True:
 
     classIds, confs, bbox = net.detect(frame, confThreshold=detection_threshold)
     print(f'{classIds=}, {bbox=}')
-    object_ids = object_tracker.update(bbox)
-    # print (f'{object_ids=}')
-
-    if len(classIds) != 0:
-        index = 0
-        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            if classNames[classId - 1] == 'person':
-                cv2.rectangle(frame, box, color=(0, 255, 0), thickness=2)
-                # cv2.putText(frame, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
-                #             cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 2)
-                # cv2.putText(frame, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
-                #             cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 2)
-                # cv2.putText(frame, "Id:" + str(object_id), (box[0] + 2, box[1] + 3),
-                #             cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 2)
+    ret, bbox = mosse_tracker.track(frame)
+    if ret:
+        # Tracking success
+        x, y, w, h = [int(i) for i in bbox]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, f"ID: {mosse_tracker.object_id - 1}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                    (255, 0, 0), 2)
+    else:
+        # Tracking failure
+        cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+    #
+    # if len(classIds) != 0:
+    #     index = 0
+    #     for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+    #         if classNames[classId - 1] == 'person':
+    #             cv2.rectangle(frame, box, color=(0, 255, 0), thickness=2)
+    #             # cv2.putText(frame, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+    #             #             cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 2)
+    #             # cv2.putText(frame, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+    #             #             cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 2)
+    #             # cv2.putText(frame, "Id:" + str(object_id), (box[0] + 2, box[1] + 3),
+    #             #             cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 2)
 
     cv2.imshow("Output", frame)
     if cv2.waitKey(1) == 27:  # Esc key
