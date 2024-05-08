7641b4d66cd189cd64ef05eaa292e4dc2ebf7620
tdh <tdhster@gmail.com>
2024-05-08 19:18:44 +0300

    Line tracking nearest object to center

--- object_tracker.py
+++ object_tracker.py
@@ -1,6 +1,53 @@
 import cv2
 import numpy as np
 
+
+class CentroidTracker:
+    def __init__(self, max_disappeared_frames=5, distance_threshold=5):
+        self.next_object_id = 0
+        self.objects = {}
+        self.disappeared = {}
+        self.disappeared_frames = {}
+        self.max_disappeared_frames = max_disappeared_frames
+        self.distance_threshold = distance_threshold
+
+    def register(self, centroid):
+        """Registers a new object with its centroid."""
+        self.objects[self.next_object_id] = centroid
+        self.disappeared_frames[self.next_object_id] = 0
+        self.next_object_id += 1
+
+    def deregister(self, object_id):
+        """Deregisters an object from tracking."""
+        del self.objects[object_id]
+        del self.disappeared_frames[object_id]
+
+    def update(self, rects):
+        """Updates the tracks of detected objects."""
+        new_objects = {}
+        for i, (x, y, w, h) in enumerate(rects):
+            centroid = (int((x + x + w) / 2), int((y + y + h) / 2), w, h)
+            object_id = -1
+            for id, center in self.objects.items():
+                # Convert tuples to NumPy arrays for element-wise subtraction
+                center_array = np.array(center)
+                centroid_array = np.array(centroid)
+                distance = np.linalg.norm(center_array - centroid_array)
+                if distance < self.distance_threshold:
+                    object_id = id
+                    self.disappeared[id] = 0
+                    break
+            if object_id < 0:
+                self.register(centroid)
+                object_id = self.next_object_id - 1
+
+            new_objects[object_id] = centroid
+
+        self.objects = new_objects
+        print(f'{new_objects=}')
+        return new_objects
+
+
 class MOSSETracker:
     def __init__(self, adaptive=False):
         self.adaptive = adaptive
@@ -34,48 +81,3 @@ class MOSSETracker:
             if np.array_equal(bbox, bbox_ref):
                 return object_id
         return -1
-
-class CentroidTracker:
-    def __init__(self, max_disappeared_frames=5, distance_threshold=5):
-        self.next_object_id = 0
-        self.objects = {}
-        self.disappeared = {}
-        self.disappeared_frames = {}
-        self.max_disappeared_frames = max_disappeared_frames
-        self.distance_threshold = distance_threshold
-
-    def register(self, centroid):
-        """Registers a new object with its centroid."""
-        self.objects[self.next_object_id] = centroid
-        self.disappeared_frames[self.next_object_id] = 0
-        self.next_object_id += 1
-
-    def deregister(self, object_id):
-        """Deregisters an object from tracking."""
-        del self.objects[object_id]
-        del self.disappeared_frames[object_id]
-
-    def update(self, rects):
-        """Updates the tracks of detected objects."""
-        new_objects = {}
-        for i, (x, y, w, h) in enumerate(rects):
-            centroid = (int((x + x + w) / 2), int((y + y + h) / 2))
-            object_id = -1
-            for id, center in self.objects.items():
-                # Convert tuples to NumPy arrays for element-wise subtraction
-                center_array = np.array(center)
-                centroid_array = np.array(centroid)
-                distance = np.linalg.norm(center_array - centroid_array)
-                if distance < self.distance_threshold:
-                    object_id = id
-                    self.disappeared[id] = 0
-                    break
-            if object_id < 0:
-                self.register(centroid)
-                object_id = self.next_object_id - 1
-
-            new_objects[object_id] = centroid
-
-        self.objects = new_objects
-        print(f'{new_objects=}')
-        return new_objects
--- track_targets.py
+++ track_targets.py
@@ -1,5 +1,7 @@
 import cv2
 from object_tracker import CentroidTracker
+import math
+import heapq
 
 
 video_path = 'test_videos/6387-191695740.mp4'  # Commercial from top
@@ -26,12 +28,36 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, INPUT_VIDEO_HEIGHT)
 cap.set(cv2.CAP_PROP_FPS, INPUT_VIDEO_FPS)
 
 
+def filter_by_target_class_id(classIds, bbox, target_class_name='person'):
+    if len(classIds):
+        # Efficient filtering using boolean indexing
+        keep_indices = classIds == classNames.index(target_class_name) + 1  # Indices where specified class ID (1 is person)
+        classIds = classIds[keep_indices]
+        bbox = bbox[keep_indices]
+        return classIds, bbox
+
+
+def find_nearest_object_id(objects):
+    # Initialize an empty priority queue
+    pq = []
+    # Calculate distances and add them to the priority queue
+    for object_id, object_data in objects.items():
+        object_x, object_y = object_data[:2]
+        distance = math.sqrt((object_x - INPUT_VIDEO_WIDTH/2)**2 + (object_y - INPUT_VIDEO_HEIGHT/2)**2)
+        heapq.heappush(pq, (distance, object_id))
+
+    # Extract the nearest object ID from the priority queue (smallest distance)
+    nearest_distance, nearest_object_id = heapq.heappop(pq)
+
+    return nearest_object_id
+
+
 classNames = []
 classFile = 'neuronet/coco.names'
 with open(classFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
-print(f'{classNames=}')
+# print(f'{classNames=}')
 configPath = 'neuronet/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'neuronet/frozen_inference_graph.pb'
 
@@ -52,21 +78,24 @@ while True:
     classIds, confs, bbox = net.detect(frame, confThreshold=detection_threshold)
     print(f'{classIds=}, {bbox=}')
 
-    if len(classIds):
-        # Efficient filtering using boolean indexing
-        keep_indices = classIds == classNames.index('person') + 1  # Indices where specified class ID (1 is person)
-        classIds = classIds[keep_indices]
-        bbox = bbox[keep_indices]
+    classIds, bbox = filter_by_target_class_id(classIds, bbox, target_class_name='person')
 
     objects = object_tracker.update(bbox)
 
     print(f'{objects=}')
     if objects:
-        for object_id, (x, y) in objects.items():
-            # draw both the ID of the object and the centroid of the
-            # object on the output frame
-            cv2.putText(frame, f'{object_id}', (x - 10, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+        object_id_near_center = find_nearest_object_id(objects)
+        for object_id, (x, y, w, h) in objects.items():
+            print(f'{object_id, (x, y, w, h)}')
+            if object_id == object_id_near_center:
+                cv2.putText(frame, f'{object_id}', (x - 10, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
+                cv2.line(frame, (int(INPUT_VIDEO_WIDTH/2), int(INPUT_VIDEO_HEIGHT/2)), (x, y), (0, 0, 255), thickness=2)
+
+
+            else:
+                cv2.putText(frame, f'{object_id}', (x - 10, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
             # cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
     cv2.imshow("Output", frame)
