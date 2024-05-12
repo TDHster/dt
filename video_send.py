ea4ca7df2b1e58e5f46b6fdb5a16fe43b5d58e3b
tdh <tdhster@gmail.com>
2024-05-12 12:46:57 +0300

    Video sender in class beautify

--- video_send.py
+++ video_send.py
@@ -3,38 +3,49 @@ import socket
 import numpy as np
 import time
 
-# Define camera index (replace with your camera index)
-camera_index = 0
+class VideoStreamSender:
+    def __init__(self, reciever_ip="192.168.0.169", server_port=5000):
+        # Define socket parameters (replace with receiver's IP address)
+        self.server_ip = reciever_ip  # Replace with receiver's IP address
+        self.server_port = server_port
 
-# Initialize video capture
-cap = cv2.VideoCapture(camera_index)
+        # Create socket object
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Define socket parameters (replace with receiver's IP address)
-server_ip = "192.168.1.169"  # Replace with receiver's IP address
-server_port = 5000
+        # Connect to the receiver
+        self.sock.connect((reciever_ip, server_port))
 
-# Create socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def send_frame(self, open_cv_frame):
+        # frame = cv2.resize(open_cv_frame, (320, 240))  # Reduce resolution
+        # Encode the frame using JPEG compression (adjust quality for balance)
+        frame_encoded = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])[1]
+        # Send the encoded frame to the receiver
+        self.sock.sendall(frame_encoded)
 
-# Connect to the receiver
-sock.connect((server_ip, server_port))
+    def make_time_delay(self, value_in_seconds=0.05):
+        # Adjust send interval based on frame rate and network conditions
+        time.sleep(value_in_seconds)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    def close(self):
+        self.sock.close()
 
-    # Resize the frame (optional)
-    resized_frame = cv2.resize(frame, (320, 240))  # Reduce resolution
 
-    # Encode the frame using JPEG compression (adjust quality for balance)
-    frame_encoded = cv2.imencode('.jpg', resized_frame, [cv2.IMWRITE_JPEG_QUALITY, 80])[1]
 
-    # Send the encoded frame to the receiver
-    sock.sendall(frame_encoded)
+if __name__ == '__main__':
+    # Define camera index (replace with your camera index)
+    camera_index = 0
 
-    # Adjust send interval based on frame rate and network conditions
-    time.sleep(0.05)  # Adjust as needed
+    # Initialize video capture
+    cap = cv2.VideoCapture(camera_index)
 
-# Close the socket and video capture when finished
-sock.close()
-cap.release()
+    video_sender = VideoStreamSender(reciever_ip="192.168.0.169", server_port=5000)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (320, 240))  # Reduce resolution
+        video_sender.send_frame(resized_frame)
+        video_sender.make_time_delay(0.05)  # Adjust as needed
+
+    video_sender.close()
+    cap.release()
