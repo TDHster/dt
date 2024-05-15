d3f396cc3d00a931fcd2505ef5e0a9e42a99b1c1
tdh <tdhster@gmail.com>
2024-05-15 15:51:37 +0300

    try fix missing frame 3

--- drone_main.py
+++ drone_main.py
@@ -78,7 +78,7 @@ while True:
     frame = cv2.resize(frame, (320, 200), interpolation=cv2.INTER_AREA)
     if not success:  # Check success flag
         continue
-    classIds, confs, bbox = object_detector.detect_objects(frame, confThreshold=detection_threshold)
+    classIds, confs, bbox = object_detector.detect_objects(frame=frame, confThreshold=detection_threshold)
     print(f'classIds={classIds}, bbox={bbox}')
 
     classIds, bbox = filter_by_target_class_id(classIds, bbox,
