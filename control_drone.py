d4c2cbcfffeba564e07594c406ba0dde109206d4
tdh <tdhster@gmail.com>
2024-05-21 16:30:01 +0300

    test three steps throttle loop

--- control_drone.py
+++ control_drone.py
@@ -288,9 +288,15 @@ if __name__ == '__main__':
     while True:
         try:
             # dron_control.yaw = sin(time_ns()/10)
-            dron_control.throttle = sin(time_ns()/10)
-            print(f' {sin(time_ns())}:0.2f')
-            sleep(1/30)
+            # dron_control.throttle = sin(time_ns()/10)
+            # print(f' {sin(time_ns())}:0.2f')
+            # sleep(1/30)
+            dron_control.throttle = 1
+            sleep(2)
+            dron_control.throttle = 0.5
+            sleep(2)
+            dron_control.throttle = 0.1
+            sleep(2)
         except KeyboardInterrupt:
             dron_control.disarm()
             break
