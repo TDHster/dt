bd121db6e8891a71bb28e4aba846cd81d1b88c08
tdh <tdhster@gmail.com>
2024-06-02 16:21:05 +0300

    yaw pid 0.1

--- main_drone.py
+++ main_drone.py
@@ -45,7 +45,7 @@ parser.add_argument(
     "-pidz", type=float, default=0.1, help="PID_Z (throttle) for drone control.", metavar='VALUE'
 )
 parser.add_argument(
-    "-pidyaw", type=float, default=0.9, help="PID_YAW for drone control.", metavar='VALUE'
+    "-pidyaw", type=float, default=0.1, help="PID_YAW for drone control.", metavar='VALUE'
 )
 parser.add_argument(
     "-dt", "--detection_threshold", type=float, default=0.45, help="detection_threshold for drone control.", metavar='VALUE'
@@ -253,9 +253,9 @@ while True:
             elif command == "Yaw right":
                 drone.yaw(0.05)
             elif command == "Move up":
-                drone.thrust(0.1)
+                drone.thrust(0.2)
             elif command == "Move down":
-                drone.thrust(-0.1)
+                drone.thrust(-0.2)
             elif command == "Land":
                 drone.manual_land()
                 drone.disarm()
