0b14bf8b200df410b99672933df8204550707cec
tdh <tdhster@gmail.com>
2024-06-03 18:25:06 +0300

    invers z axis

--- main_drone.py
+++ main_drone.py
@@ -248,7 +248,7 @@ while True:
                 drone.yaw = yaw_pixels/INPUT_VIDEO_WIDTH * PID_YAW  # need correction factor  *diagonal/image_diagonal
                 dz = elevation_pixels/INPUT_VIDEO_HEIGHT * PID_Z
                 print(f'{bcolors.WARNING}{elevation_pixels=}\t{dz}{bcolors.ENDC}')
-                drone.thrust = -dz
+                drone.thrust = dz
 
             elif object_id == object_id_near_center:
                 # cv2.putText(frame, f'{object_id}', (x - 10, y - 10),
