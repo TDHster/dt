fa149f1102e3922ef0956e15d83b4eacbda7c7a5
tdh <tdhster@gmail.com>
2024-06-04 16:03:44 +0300

    AUTO mode start (fixed)

--- mavlink_pwm_control.py
+++ mavlink_pwm_control.py
@@ -242,8 +242,8 @@ class MavlinkDrone:
 
     def takeoff_via_mavlink(self, takeoff_altitude):
         # self.mode_alt_hold()
-        self._set_mode('GUIDED_NOGPS')
-        # self._set_mode('AUTO')
+        # self._set_mode('GUIDED_NOGPS')
+        self._set_mode('AUTO')
         sleep(0.5)
         self.arm()
         sleep(2)
