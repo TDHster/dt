6831bc47de6b57756246dfcdc6c30c27565c8a15
tdh <tdhster@gmail.com>
2024-06-08 17:32:47 +0300

    pitch, roll, throttle, yaw = 0, 0, 0.6, 0

--- mavsdk/manual_control.py
+++ mavsdk/manual_control.py
@@ -65,7 +65,7 @@ async def manual_controls():
 
     # Takeoff the vehicle
     print("-- Taking off")
-    await drone.action.takeoff()
+    # await drone.action.takeoff()
     await asyncio.sleep(5)
 
     # set the manual control input after arming
@@ -97,7 +97,7 @@ async def manual_controls():
             # yaw = float(input_list[3])
 
             # await drone.manual_control.set_manual_control_input(pitch, roll, throttle, yaw)
-            pitch, roll, throttle, yaw = 0, 0, 0.6, 0
+            pitch, roll, throttle, yaw = 0, 0, 0.6, 0.5
             await drone.manual_control.set_manual_control_input(pitch, roll, throttle_value, yaw)
 
             await asyncio.sleep(0.1)
