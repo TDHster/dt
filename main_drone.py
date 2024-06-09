fe607d508c0faa576ad6da1317d6a3a2d7d6dcf0
tdh <tdhster@gmail.com>
2024-06-09 16:50:09 +0300

    pid z

--- main_drone.py
+++ main_drone.py
@@ -49,13 +49,13 @@ parser.add_argument(
 parser.add_argument(
     "-pidx", type=float, default=0.4, help="PID_X for drone control.", metavar='VALUE'
 )
-# 0.1 0.3 0.4 0.6 0.7 0.8 0.9 0.7 0.6
+# 0.1 0.3 0.4 0.6 0.7 0.8 0.9 0.7 0.6 0.5
 parser.add_argument(
-    "-pidz", type=float, default=0.5, help="PID_Z (throttle) for drone control.", metavar='VALUE'
+    "-pidz", type=float, default=0.7, help="PID_Z (throttle) for drone control.", metavar='VALUE'
 )
 # 0.45 0.48 0.5 0.55 #correction formula 0.8, 0.5, 0.4, 0.3
 parser.add_argument(
-    "-pidyaw", type=float, default=0.3, help="PID_YAW for drone control.", metavar='VALUE'
+    "-pidyaw", type=float, default=0.35, help="PID_YAW for drone control.", metavar='VALUE'
 )
 parser.add_argument(
     "-dt", "--detection_threshold", type=float, default=0.45, help="detection_threshold for drone control.", metavar='VALUE'
