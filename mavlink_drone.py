5de9d5c326f0e0f5d2878e2e52293ddc47e24663
tdh <tdhster@gmail.com>
2024-06-15 17:23:31 +0400

    tests

--- mavlink_drone.py
+++ mavlink_drone.py
@@ -200,8 +200,8 @@ class MavlinkDrone:
 
         if not yaw_rate:
             # yaw_rate_PID = 0.75
-            # yaw_rate = abs(yaw) * yaw_rate_PID
-            yaw_rate = 40
+            yaw_rate = abs(yaw) * 5
+            # yaw_rate = 40
         if yaw >= 0:
             direction = 1
         else:
