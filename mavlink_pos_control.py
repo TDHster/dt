152684ba0248ca91ee3ae5481eacd4b90fc7af36
tdh <tdhster@gmail.com>
2024-06-18 13:19:40 +0400

    fly tests

--- mavlink_pos_control.py
+++ mavlink_pos_control.py
@@ -256,8 +256,8 @@ class MavlinkDrone:
 
         yaw_angle = abs(yaw_angle) * pi / 180
 
-        YAW_CHANGE_RELATIVE = 0
-        # YAW_CHANGE_ABSOLUTE = 1
+        # YAW_CHANGE_RELATIVE = 0
+        YAW_CHANGE_ABSOLUTE = 1
 
         abs_rel_flag = YAW_CHANGE_RELATIVE  # 0 - relative, 1 - abs(0 - is North)
         print(f'Debug: {yaw_angle=}\t{yaw_rate=}')
